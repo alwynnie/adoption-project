2d8eda9bce839319d3c936f164d9f88c71463b83
alwynnie <mybff308@gmail.com>
2025-07-02 16:07:53 -0300

    index, login and register added with styles

--- adoptionapp/urls.py
+++ adoptionapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from .views import index, register, login
 
 urlpatterns = [
     #path('dashboard/', views.dashboard)
+    path('', index, name='home'),
+    path('register/', register, name='register'),
+    path('login/', login, name='login')
 ]
